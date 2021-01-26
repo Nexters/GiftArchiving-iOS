1681f0be013634fecf146e2492c66dadba0bba7a
jaeyong <maxwodyd98@gmail.com>
2021-01-26 14:34:02 +0900

    create: UIColor extension 메인 컬러 추가

---
+++ Gift.zip-iOS/Record/VCs/RecordVC.swift
@@ -0,0 +1,29 @@
+//
+//  RecordVC.swift
+//  Gift.zip-iOS
+//
+//  Created by 이재용 on 2021/01/26.
+//
+
+import UIKit
+
+class RecordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
