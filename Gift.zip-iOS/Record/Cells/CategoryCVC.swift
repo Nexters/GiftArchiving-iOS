738bb82461b28134b9cb8fc84a9c6f47fe0aebfb
jaeyong <maxwodyd98@gmail.com>
2021-02-04 05:02:10 +0900

    fix: MainSB 파일 교체

--- Gift.zip-iOS/Record/Cells/CategoryCVC.swift
+++ Gift.zip-iOS/Record/Cells/CategoryCVC.swift
@@ -11,10 +11,26 @@ class CategoryCVC: UICollectionViewCell {
     static let identifier: String = "CategoryCVC"
     
     @IBOutlet var buttons: [UIButton]!
-
+   
+    @IBOutlet var labels: [UILabel]!
+   
+    @IBOutlet var images: [UIImageView]!
+    
+    var popupBackgroundColor: UIColor?
+    
     func setBorder() {
         for button in buttons {
             button.makeRounded(cornerRadius: 8.0)
+        }
+    }
+    
+    func setCategories() {
+        let labels = contentView.subviews.filter {
+            $0 is UILabel
+        }
+        
+        for label in labels {
+            
         }
     }
     
