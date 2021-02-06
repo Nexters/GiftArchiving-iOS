0bb9bdf0b3dff686825a7e8bc0b36cfbaeecd22d
jaeyong <maxwodyd98@gmail.com>
2021-02-06 18:58:00 +0900

    create: setting SB,VC 추가

---
+++ Gift.zip-iOS/Settings/VCs/SettingsVC.swift
@@ -0,0 +1,18 @@
+//
+//  SettingVC.swift
+//  Gift.zip-iOS
+//
+//  Created by 이재용 on 2021/02/06.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+}
