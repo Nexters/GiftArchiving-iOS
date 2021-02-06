6edfedf11115bb19103311eb437ac6e3336404a4
jaeyong <maxwodyd98@gmail.com>
2021-02-06 19:28:15 +0900

    update: 설정 화면 UI 구성

---
+++ Gift.zip-iOS/Settings/Cells/SettingsTVC.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsTVC.swift
+//  Gift.zip-iOS
+//
+//  Created by 이재용 on 2021/02/06.
+//
+
+import UIKit
+
+class SettingsTVC: UITableViewCell {
+    static let identifier: String = "SettingsTVC"
+    @IBOutlet weak var settingIconImageView: UIImageView!
+    @IBOutlet weak var settingNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func setInformations(settingIconName: String, settingIconImageName: String) {
+        settingNameLabel.text = settingIconImageName
+        settingIconImageView.image = UIImage.init(named: settingIconImageName)
+    }
+}
--- Gift.zip-iOS/Settings/VCs/SettingsVC.swift
+++ Gift.zip-iOS/Settings/VCs/SettingsVC.swift
@@ -9,10 +9,42 @@ import UIKit
 
 class SettingsVC: UIViewController {
 
+   
+    @IBOutlet weak var settingTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+initDelegate()
     }
+    
+    private func initDelegate() {
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+    }
+}
+
+extension SettingsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else if section == 1 {
+            return 2
+        } else {
+            return 3
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTVC.identifier, for: indexPath) as? SettingsTVC else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
     
 }
