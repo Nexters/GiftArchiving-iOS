b3c164fc1c91a026377f24782b73654e02aab500
Jeong-heon2 <cjhgg2@gmail.com>
2021-02-13 20:25:06 +0900

    create: load gift api service 생성 #42

---
+++ Gift.zip-iOS/APIService/Load/LoadGiftListService.swift
@@ -0,0 +1,14 @@
+//
+//  LoadGiftListService.swift
+//  Gift.zip-iOS
+//
+//  Created by Choi jeong heon on 2021/02/13.
+//
+
+import Foundation
+import Alamofire
+
+struct LoadGiftListService{
+    static let shared = LoadGiftListService()
+    
+}
