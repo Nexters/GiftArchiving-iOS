0ccf79f98f645f9b9c70c478b5afbe011f5720d4
jaeyong <maxwodyd98@gmail.com>
2021-02-19 06:35:41 +0900

    update: 작성하기 도중 나가기 팝업 추가

--- Gift.zip-iOS/Record/VCs/WarningOutVC.swift
+++ Gift.zip-iOS/Record/VCs/WarningOutVC.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class WarningOutVC: UIViewController {
 
+    @IBOutlet weak var roundView: UIView!
+    @IBOutlet var buttons: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        roundView.roundCorners(cornerRadius: 8.0, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
+        for button in buttons {
+            button.makeRounded(cornerRadius: 8.0)
+        }
+        
+    }
+    @IBAction func dismiss(_ sender: Any) {
+        NotificationCenter.default.post(name: .init("goOutToMain"), object: nil)
+        self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func goOut(_ sender: Any) {
+        NotificationCenter.default.post(name: .init("goOutToMain"), object: nil, userInfo: ["cancel": true])
+        self.dismiss(animated: true) {
+            
+        }
+    }
 }
