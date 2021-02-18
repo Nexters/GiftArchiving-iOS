e5d17d815de8d6186b9024746e65a084b9f1fe91
jaeyong <maxwodyd98@gmail.com>
2021-02-19 04:48:43 +0900

    update: 수정하기 api 붙임..

--- Gift.zip-iOS/Detail/VCs/DetailVC.swift
+++ Gift.zip-iOS/Detail/VCs/DetailVC.swift
@@ -11,6 +11,7 @@ import Kingfisher
 class DetailVC: UIViewController {
     
     @IBOutlet weak var upperContainer: UIView!
+    @IBOutlet weak var topSafeAreaView: UIView!
     
     @IBOutlet weak var isReceiveLabel: UILabel!
     @IBOutlet weak var giftImageView: UIImageView!
@@ -24,14 +25,46 @@ class DetailVC: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var nameLabel: UITextField!
     @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
     
-
+    
     @IBOutlet weak var moreButton: UIButton!
     @IBOutlet weak var exitButton: UIButton!
     @IBOutlet var textsToChangeColor: [UILabel]!
     
     var giftId: String = "602d16ef00eec17e8db2b4e9"
     var currentBackgroundColor: String = "charcoalGrey"
+    
+    lazy var popupBackground = UIView()
+    
+    private var giftImage: UIImage?
+    private var bgImgURL: String?
+    private var receiveDate: String?
+    private var isReceiveGift: Bool = true
+    private var isGiftEditing: Bool = false {
+        didSet {
+            nameLabel.isUserInteractionEnabled = false
+            if isGiftEditing {
+                contentTextView.isUserInteractionEnabled = true
+                for button in tagButtons {
+                    button.isUserInteractionEnabled = true
+                }
+                
+                let image = currentBackgroundColor == "wheat" ? "iconCheckBk" : "iconCheck"
+                moreButton.setImage(UIImage(named: image), for: .normal)
+                
+            } else {
+                contentTextView.isUserInteractionEnabled = false
+                for button in tagButtons {
+                    button.isUserInteractionEnabled = false
+                }
+                let image = currentBackgroundColor == "wheat" ? "iconMoreBk" : "iconMore"
+                moreButton.setImage(UIImage(named: image), for: .normal)
+            }
+        }
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -40,7 +73,8 @@ class DetailVC: UIViewController {
             switch result {
             case .success(let data):
                 guard let gift = data as? GiftModel else { return }
-                
+                print(self.giftId)
+                print(gift.receiveDate)
                 if gift.bgColor == "wheat" {
                     for label in self.textsToChangeColor {
                         label.textColor = .black
@@ -48,10 +82,17 @@ class DetailVC: UIViewController {
                     self.contentTextView.textColor = .black
                     self.dateLabel.textColor = .black
                     self.nameLabel.textColor = .black
-                    
+                    let exitImage = UIImage(named: "iconCancelBk")
+                    self.exitButton.setImage(exitImage, for: .normal)
+                    let moreImage = UIImage(named: "iconMoreBk")
+                    self.moreButton.setImage(moreImage, for: .normal)
                 } else {
                     
                 }
+                self.currentBackgroundColor = gift.bgColor
+                self.bgImgURL = gift.bgImgURL
+                self.receiveDate = gift.receiveDate
+                self.isReceiveGift = gift.isReceiveGift
                 self.upperContainer.backgroundColor = UIColor.init(named: gift.bgColor)
                 self.view.backgroundColor = UIColor.init(named: gift.bgColor)
                 self.setPageInformation(
@@ -89,28 +130,75 @@ class DetailVC: UIViewController {
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         for btn in tagButtons {
             btn.makeRounded(cornerRadius: 8.0)
         }
+        isGiftEditing = false
+        popupBackground.setPopupBackgroundView(to: view)
+        NotificationCenter.default.addObserver(self, selector: #selector(selectIcon), name: .init("selectIcon"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(popupChange), name: .init("popupchange"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(broadcastDelete), name: .init("broadcastDelete"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(broadcastUpdate(_:)), name: .init("broadcastUpdate"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(editGift), name: .init("editGift"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(shareGift), name: .init("shareGift"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
+    
+    
+    //    private func
+    @objc private func keyboardWillShow(_ sender: Notification) {
+        handleKeyboardIssue(sender, isAppearing: true)
+    }
+    
+    @objc private func keyboardWillHide(_ sender: Notification) {
+        handleKeyboardIssue(sender, isAppearing: false)
+    }
+    
+    private func handleKeyboardIssue(_ notification: Notification, isAppearing: Bool) {
+        guard let userInfo = notification.userInfo as? [String: Any] else { return }
+        guard let keyboardAnimationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        guard let keyboardHeight = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height else { return
+        }
+        // 기기별 bottom safearea 계산하기
+        let heightConstant = isAppearing ? keyboardHeight - 21 : 13
+        
+        UIView.animate(withDuration: keyboardAnimationDuration) {
+            
+            if isAppearing {
+                self.topConstraint.priority = UILayoutPriority(rawValue: 248)
+            } else {
+                self.topConstraint.priority = UILayoutPriority(rawValue: 1000)
+            }
+            self.bottomConstraint.constant = heightConstant
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         NotificationCenter.default.removeObserver(self, name: .init("broadcastUpdate"), object: nil)
         NotificationCenter.default.removeObserver(self, name: .init("popupchange"), object: nil)
         NotificationCenter.default.removeObserver(self, name: .init("broadcastDelete"), object: nil)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     // UI 작업
     private func setPageInformation(imageURL : String, name: String, receiveDate: String, isReceiveGift: Bool,  category: String, emotion: String, reason: String, content: String, bgColor: String) {
-
+        
         let url = URL(string: imageURL.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed)!) //
         giftImageView.kf.setImage(with: url)
+        giftImage = giftImageView.image
         nameLabel.text = name
         isReceiveLabel.text = isReceiveGift ? "From." : "To."
         let dateBeforeParsing: String = receiveDate.components(separatedBy: "T")[0]
@@ -122,7 +210,7 @@ class DetailVC: UIViewController {
         let dateAfterParsing = formatter.string(from: date!)
         let dateToRecord = dateAfterParsing + getDayOfWeek(date!)
         dateLabel.text = dateToRecord
-        
+        topSafeAreaView.backgroundColor = UIColor.init(named: bgColor)
         var categoryName: String = "카테고리"
         var purposeName: String = "목적"
         var emotionName: String = "감정"
@@ -209,19 +297,156 @@ class DetailVC: UIViewController {
         }
     }
     
+    
+    @objc private func selectIcon(_ notification: Notification) {
+        
+        popupBackground.animatePopupBackground(false)
+        view.bringSubviewToFront(popupBackground)
+        
+        guard let userInfo = notification.userInfo as? [String: Any] else { return }
+        guard let iconImage = userInfo["iconImageName"] as? String else { return }
+        guard let iconName = userInfo["iconName"] as? String else { return }
+        guard let iconKind = userInfo["iconKind"] as? String else { return }
+        
+        if iconKind == "category" {
+            categoryImageView.image = UIImage(named: iconImage)
+            categoryLabel.text = iconName
+        } else if iconKind == "purpose" {
+            purposeImageView.image = UIImage(named: iconImage)
+            purposeLabel.text = iconName
+        } else {
+            feelingImageView.image = UIImage(named: iconImage)
+            emotionLabel.text = iconName
+        }
+    }
+    
+    
+    @objc func shareGift() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.11) {
+            
+            let sb = UIStoryboard.init(name: "Share", bundle: nil)
+            guard let share = sb.instantiateViewController(identifier: "ShareVC") as? ShareVC else { return }
+            share.whereToGo = 1
+            share.envelopImage = self.giftImage
+            
+            share.currentName = "\(self.isReceiveLabel.text!) \(self.nameLabel.text!)"
+            share.currentBackgroundColor = UIColor(named: self.currentBackgroundColor)
+            share.kakaoImageURL = self.bgImgURL
+            //            share.instagramImage = instagramSquareImage
+            //            // instagramSquareImage
+            //            share.myPhonePhoto = myPhonePhoto
+            //            // myPhonePhoto
+            //            share.currentFrameOfImage = currentFrameOfImage
+            //            share.userName = nameTextField.text
+            share.modalPresentationStyle = .fullScreen
+            self.present(share, animated: true, completion: nil)
+        }
+    }
+    
+    @objc func editGift() {
+        
+        isGiftEditing = true
+    }
+    
     @objc func popupChange() {
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.11) {
             
             guard let deleteVC = self.storyboard?.instantiateViewController(identifier: "DeleteDetailVC") as? DeleteDetailVC else { return }
-            
             deleteVC.giftId = self.giftId
             deleteVC.modalPresentationStyle = .overCurrentContext
             self.present(deleteVC, animated: true, completion: nil)
-            
         }
+    }
+    
+    @IBAction func categoryButtonTapped(_ sender: Any) {
+        popupBackground.animatePopupBackground(true)
+        guard let vc = UIStoryboard.init(name: "Record", bundle: nil).instantiateViewController(identifier: "IconPopupVC") as? IconPopupVC else { return }
+        view.bringSubviewToFront(categoryLabel)
+        view.bringSubviewToFront(categoryImageView)
+        vc.whichPopup = 0
+        var color: UIColor?
+        switch currentBackgroundColor {
+        case "charcoalGrey":
+            color = .charcoalGrey
+            break
+        case "ceruleanBlue":
+            color = .ceruleanBlue
+            break
+        case "wheat":
+            color = .wheat
+            break
+        case "pinkishOrange":
+            color = .pinkishOrange
+            break
+        default:
+            break
+        }
+        vc.backgroundColor = color
+        vc.popupViewHeightByPhones = self.view.frame.height - categoryLabel.frame.maxY - 49
+        vc.modalPresentationStyle = .overCurrentContext
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction func purposeButtonTapped(_ sender: Any) {
         
+        popupBackground.animatePopupBackground(true)
+        guard let vc = UIStoryboard.init(name: "Record", bundle: nil).instantiateViewController(identifier: "IconPopupVC") as? IconPopupVC else { return }
+        view.bringSubviewToFront(purposeLabel)
+        view.bringSubviewToFront(purposeImageView)
+        vc.whichPopup = 1
+        var color: UIColor?
+        switch currentBackgroundColor {
+        case "charcoalGrey":
+            color = .charcoalGrey
+            break
+        case "ceruleanBlue":
+            color = .ceruleanBlue
+            break
+        case "wheat":
+            color = .wheat
+            break
+        case "pinkishOrange":
+            color = .pinkishOrange
+            break
+        default:
+            break
+        }
+        vc.backgroundColor = color
+        vc.popupViewHeightByPhones = self.view.frame.height - categoryLabel.frame.maxY - 49
+        vc.modalPresentationStyle = .overCurrentContext
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction func emotionButtonTapped(_ sender: Any) {
         
+        popupBackground.animatePopupBackground(true)
+        guard let vc = UIStoryboard.init(name: "Record", bundle: nil).instantiateViewController(identifier: "IconPopupVC") as? IconPopupVC else { return }
+        view.bringSubviewToFront(emotionLabel)
+        view.bringSubviewToFront(feelingImageView)
+        vc.whichPopup = 2
+        var color: UIColor?
+        switch currentBackgroundColor {
+        case "charcoalGrey":
+            color = .charcoalGrey
+            break
+        case "ceruleanBlue":
+            color = .ceruleanBlue
+            break
+        case "wheat":
+            color = .wheat
+            break
+        case "pinkishOrange":
+            color = .pinkishOrange
+            break
+        default:
+            break
+        }
+        vc.backgroundColor = color
+        vc.popupViewHeightByPhones = self.view.frame.height - categoryLabel.frame.maxY - 49
+        vc.isReceiveGift = isReceiveGift
+        vc.modalPresentationStyle = .overCurrentContext
+        self.present(vc, animated: true, completion: nil)
     }
     
     @IBAction func dismissDetail(_ sender: Any) {
@@ -229,12 +454,75 @@ class DetailVC: UIViewController {
     }
     
     @IBAction func moreButtonTapped(_ sender: Any) {
+        if isGiftEditing {
+            // 통신
+            if !contentTextView.text.isEmpty {
+                var category: String = ""
+                var emotion: String = ""
+                var reason: String = ""
+                let content: String = contentTextView.text
+                
+                for c in Icons.category {
+                    if categoryLabel.text == c.name {
+                        category = c.englishName
+                    }
+                }
+                if isReceiveGift {
+                    for e in Icons.emotionGet {
+                        if emotionLabel.text == e.name {
+                            emotion = e.englishName
+                        }
+                    }
+                } else {
+                    for e in Icons.emotionSend {
+                        if emotionLabel.text == e.name {
+                            emotion = e.englishName
+                        }
+                    }
+                }
+                for p in Icons.purpose {
+                    if purposeLabel.text == p.name {
+                        reason = p.englishName
+                    }
+                }
+
+                GiftService.shared.putGift(category: category, content: content, emotion: emotion, reason: reason, receiveDate: receiveDate!, giftId: giftId)  { networkResult -> Void in
+                    switch networkResult {
+                    case .success:
+                        print("ㄷ ㄷ")
+                    case .requestErr:
+                        let alertViewController = UIAlertController(title: "통신 실패", message: "💩", preferredStyle: .alert)
+                        let action = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+                        alertViewController.addAction(action)
+                        self.present(alertViewController, animated: true, completion: nil)
+                        
+                    case .pathErr: print("path")
+                    case .serverErr:
+                        let alertViewController = UIAlertController(title: "통신 실패", message: "서버 오류", preferredStyle: .alert)
+                        let action = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+                        alertViewController.addAction(action)
+                        self.present(alertViewController, animated: true, completion: nil)
+                        print("networkFail")
+                        print("serverErr")
+                    case .networkFail:
+                        self.isGiftEditing = false
+                    }
+                }
+            } else {
+                let alertViewController = UIAlertController(title: "저장 실패", message: "내용을 입력해주세요. 🥰", preferredStyle: .alert)
+                let action = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+                alertViewController.addAction(action)
+                self.present(alertViewController, animated: true, completion: nil)
+            }
+            
+        } else {
+            guard let moreVC = self.storyboard?.instantiateViewController(identifier: "DetailMoreVC") as? DetailMoreVC else { return }
+            
+            moreVC.currentBackgroundColor = self.currentBackgroundColor
+            moreVC.modalPresentationStyle = .overCurrentContext
+            self.present(moreVC, animated: true, completion: nil)
+        }
         
-        guard let moreVC = self.storyboard?.instantiateViewController(identifier: "DetailMoreVC") as? DetailMoreVC else { return }
-        
-        moreVC.currentBackgroundColor = self.currentBackgroundColor
-        moreVC.modalPresentationStyle = .overCurrentContext
-        self.present(moreVC, animated: true, completion: nil)
     }
     
     //MARK: 삭제 API success 후,  로컬에서도 삭제시키기
@@ -268,6 +556,7 @@ class DetailVC: UIViewController {
         }
         self.dismiss(animated: true, completion: nil)
     }
+    
     @objc func broadcastUpdate(_ notification: Notification){
         guard let name = notification.userInfo?["name"] as? String else { return }
         guard let content = notification.userInfo?["content"] as? String else { return }
