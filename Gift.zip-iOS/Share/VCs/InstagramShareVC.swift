3506c34342ccec1e7e869a74d23d5a3698411d57
jaeyong <maxwodyd98@gmail.com>
2021-02-19 20:26:47 +0900

    update: 인스타 피드 구현

--- Gift.zip-iOS/Share/VCs/InstagramShareVC.swift
+++ Gift.zip-iOS/Share/VCs/InstagramShareVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Photos
 
 class InstagramShareVC: UIViewController, UIDocumentInteractionControllerDelegate {
     
@@ -28,23 +29,39 @@ class InstagramShareVC: UIViewController, UIDocumentInteractionControllerDelegat
     }
     
     @IBAction func shareFeed(_ sender: Any) {
-        let pngFile = changeImageToPNG(instagramImage!)
-        shareToInstagramFeed(pngFile)
+//        let pngFile = changeImageToPNG(instagramImage!)
+        postImage(image: instagramImage!)
     }
     
-    private func shareToInstagramFeed(_ imageData: Data) {
-        guard let instagramUrl = URL(string: "instagram://app") else { return }
+    
+    func postImage(image: UIImage, result:((Bool)->Void)? = nil) {
+        guard let instagramURL = URL(string: "instagram://app") else {
+            if let result = result {
+                result(false)
+            }
+            return
+        }
         
-        if UIApplication.shared.canOpenURL(instagramUrl) {
-            UIApplication.shared.open(instagramUrl, options: [:], completionHandler: nil)
-        } else {
-            // Instagram app is not installed or can't be opened, pop up an alert
-            let alert = UIAlertController(title: "알림", message: "인스타그램이 필요합니다", preferredStyle: .alert)
-            let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
-            alert.addAction(ok)
-            self.present(alert, animated: true, completion: nil)
+        do {
+            try PHPhotoLibrary.shared().performChangesAndWait {
+                let request = PHAssetChangeRequest.creationRequestForAsset(from: image)
+                
+                let assetID = request.placeholderForCreatedAsset?.localIdentifier ?? ""
+                let shareURL = "instagram://library?LocalIdentifier=" + assetID
+                
+                if UIApplication.shared.canOpenURL(instagramURL) {
+                    if let urlForRedirect = URL(string: shareURL) {
+                        UIApplication.shared.open(urlForRedirect, options: [:], completionHandler: nil)
+                    }
+                }
+            }
+        } catch {
+            if let result = result {
+                result(false)
+            }
         }
     }
+    
     
     private func changeImageToPNG(_ image: UIImage) -> Data {
         guard let imageData = image.pngData() else
